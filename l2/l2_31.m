518f909fc1003b27f8eb8416f1b9b9c261f0ab75
Viktoria <Viktoria@192.168.0.5>
2017-10-07 19:52:16 +0300

    lb2_31

---
+++ l2/l2_31.m
@@ -0,0 +1,12 @@
+clc;
+b=[1 0.618 1]; a=1; n=512; fs=200;
+[h,w]=freqz(b,a,n);
+mag=abs(h); phase=angle(h)*180/pi;
+subplot(311); plot(w/(2*pi)*fs,mag),grid on
+subplot(312), plot(w/(2*pi)*fs,unwrap(phase)),grid on
+nuli = roots(b)
+figure(2)
+zplane(nuli)
+% k(z)=1+0.618*z^-1+1*z^-2
+
+
