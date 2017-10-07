7877f2fc76917e4f7dbb5c1af9bb7e9a544674e3
Viktoria <Viktoria@192.168.0.5>
2017-10-07 18:41:15 +0300

    L2_13

---
+++ l2/l2_13.m
@@ -0,0 +1,9 @@
+clc;
+b=[0.25 0.5 0.25]; a=1;
+ecg= detrend(ecg117);
+y = filter(b, a, ecg);
+subplot(211), plot(y), xlim([1,150]), title ('Відфільтрована ектрокардіограма'), xlabel('t'), ylabel('ecg'); 
+subplot(212), plot(ecg), xlim([1,150]), title ('Початкова електрокардіограма'), xlabel('t'), ylabel('ecg'); 
+
+
+
